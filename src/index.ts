6872a9c94d0285505bc9c10b0fb87ac4d8887c3c
BachNX <thanbaiks@gmail.com>
2026-06-14 18:26:39 +0700

    Support PUBLIC_URL env var for manifest installation logs

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import app from './app';
 
 // Start node HTTP server
 const PORT = process.env.PORT || 3005;
+const PUBLIC_URL = process.env.PUBLIC_URL || `http://localhost:${PORT}`;
+
 const server = createServer(toNodeListener(app));
 server.listen(PORT, () => {
   console.log(`🚀 asmallcinema addon is running!`);
-  console.log(`👉 Install Manifest URL in Stremio: http://localhost:${PORT}/manifest.json`);
+  console.log(`👉 Install Manifest URL in Stremio: ${PUBLIC_URL}/manifest.json`);
 });

7e36c2f542c9c70dfdea9d5c68b4407f8c257cfe
BachNX <thanbaiks@gmail.com>
2026-06-15 15:07:27 +0700

    chore: bump version to 0.1.0

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,7 @@ import { ADDON_ID, ADDON_NAME } from './config';
 
 export const manifest = {
   id: ADDON_ID,
-  version: "1.0.0",
+  version: "0.1.0",
   name: ADDON_NAME,
   description: "A Stremio addon for asmallcinema.",
   resources: ["stream"],
